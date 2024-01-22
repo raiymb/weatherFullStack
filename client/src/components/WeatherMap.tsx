8e70b2b6689d93da5aa3bae67e31b9928bdfd395
Райымбек <raimhg.time@gmail.com>
2024-01-22 22:32:39 +0600

    custom marker

--- client/src/components/WeatherMap.tsx
+++ client/src/components/WeatherMap.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerImage from './images/leafletMapMarker.png'
 
 interface Coordinates {
   lat: number;
@@ -11,8 +13,16 @@ interface WeatherMapProps {
   coordinates: Coordinates;
 }
 
+
 const WeatherMap: React.FC<WeatherMapProps> = ({ coordinates }) => {
   const [mapKey, setMapKey] = useState(Math.random());
+
+  const customIcon = new L.Icon({
+    iconUrl: markerImage, 
+    iconSize: [25, 41], 
+    iconAnchor: [12, 41], 
+    popupAnchor: [1, -34], 
+  });
 
   useEffect(() => {
     setMapKey(Math.random());
@@ -29,7 +39,7 @@ const WeatherMap: React.FC<WeatherMapProps> = ({ coordinates }) => {
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[coordinates.lat, coordinates.lon]} />
+      <Marker position={[coordinates.lat, coordinates.lon]} icon={customIcon}/>
     </MapContainer>
   );
 };
--- server/app.js
+++ server/app.js
@@ -10,14 +10,14 @@ const app =express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/', wikipediaRoutes);
 app.use('/api/weather', weatherRoutes);
 app.use('/news', gnewsRoutes);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/dist/', 'index.html'));
 });
 
 app.listen(port, () => {
